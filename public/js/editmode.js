f64224781c152723d666c2d7d06600058464492f
Jorge <jorge.fulgencioesteves@heig-vd.ch>
2019-11-12 11:48:50 +0100

    Edit mode work in progress, slide creation and local storage saving

--- public/js/editmode.js
+++ public/js/editmode.js
@@ -12,13 +12,17 @@ const titleTemplate = document.getElementById("titleSlide");
 const txtAndImgTemplate = document.getElementById("txtAndImgTemplate");
 const listSlideTemplate = document.getElementById("listSlideTemplate");
 
+const slidePreviewCont = document.getElementById("allSlidesPreviewCont");
+const editSlideCont = document.getElementById("editSlideCont");
+
+
 // --- Classes ---------------------------------------------------
 
 class Slide {
     constructor(typeOfSlide) {
         this.slide = {
             type: typeOfSlide,
-            textBoxes: [{text: ["Your text here ..."]}, {text:["Your text here ..."] }],
+            textBoxes: [{text: ["Your text here ..."]}, {text: ["Your text here ..."]}],
             img: {
                 src: ""
             },
@@ -48,9 +52,9 @@ class Slide {
         }
     }
 
-getSlide(){
-    return this.slide;
-}
+    getSlide() {
+        return this.slide;
+    }
 
     addBulletList() {
         this.slide.textBoxes.push({
@@ -101,61 +105,126 @@ getSlide(){
 
 // ---------------------------------------------------------------------
 
-
 let lastClickedElm;
+let currentSlide = 0;
+let presentation = {id: 28, name: "", date: "", theme: "", slides: [], visibility: 0};
+// initialize();
 
-let presentation = JSON.parse(localStorage.getItem("presentation"));
-presentation = presentation.presentation;
+// Fetches the last modified presentation from localStorage and DB
+async function initialize() {
+    // GET request to fetch presentation in db
+    let urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get('id');
 
-if(presentation.presentation.slides.length == 0){
-    presentation.presentation.slides.push(new Slide("title"));
+    let url = `http://localhost:8080/presentations/${id}`;
+
+
+    let cfg = {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+    };
+    let dbPresentation = null;
+    let localPresentation = null;
+
+    try {
+        let resp = await fetch(url, cfg);
+        dbPresentation = await resp.json();
+    } catch (err) {
+        console.log(err);
+    }
+
+    try {
+        localPresentation = JSON.parse(localStorage.getItem("presentation"));
+
+        if (presentation.slides.length === 0) {
+            presentation.slides.push(new Slide("title"));
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+    if (dbPresentation.lastEdited <= localPresentation.lastEdited) {
+        presentation = dbPresentation;
+    } else {
+        presentation = localPresentation;
+    }
+}
+
+setup();
+function setup() {
+    for(let i = 0; i < 1; i++){
+        let slidePreviewDiv = document.createElement("div");
+        slidePreviewDiv.className = "slidePreview";
+        slidePreviewCont.appendChild(slidePreviewDiv);
+    }
+
+
+    // ------------- Sets the height and width for the edit slide div-container ---------------
+
+    let bodyHeight = document.body.clientHeight;
+
+    slidePreviewCont.style.height = bodyHeight - 80 - 30; // This sets the height for the div with the slides previews. The heghit is bodyheight-toolbarheight-padding.
+
+    let slideHeight = bodyHeight - 80 - 30;
+    let slideWidth = (slideHeight/9) * 16;
+
+    let slideMaxWidth = document.body.clientWidth - 200;
+
+    if(slideWidth > slideMaxWidth){
+        slideWidth = slideMaxWidth;
+    }
+
+    slideHeight = (slideWidth/16)*9;
+
+    editSlideCont.style.height = slideHeight;
+    editSlideCont.style.width = slideWidth;
 }
 
 // --- EventListener ---------------------------------------------------
-
 saveBtn.addEventListener('click', async evt => {
+    // TODO: Factorize it with other queries and add an independent function
     let url = "http://localhost:8080/presentations/updatePresentation";
 
-    let updata =  {
+    let updata = {
         presID: presentation.id,
-        pres: presentation.presentation
-    }
+        pres: presentation
+    };
 
     let cfg = {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(updata)
-    }
+    };
 
     try {
         let resp = await fetch(url, cfg);
         let data = await resp.json();
-        
+
         console.log(data.msg);
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
     }
-    
-})
+});
 
 newSlideBtn.addEventListener('click', evt => {
-    presentation.presentation.slides.push(new Slide("txtAndImg"));
+    editSlideCont.innerHTML = "";
+    createTxtAndImgSlide();
+    currentSlide++;
 });
 
 backBtn.addEventListener('click', evt => {
     window.location.href = "../html/overview.html";
-})
+});
 
 listSlideBtn.addEventListener('click', evt => {
     createListSlide();
-})
+});
 
 txtAndImageSlideBtn.addEventListener('click', evt => {
     createTxtAndImgSlide();
-})
+});
 
-titleSlideBtn.addEventListener('click',evt =>{
+titleSlideBtn.addEventListener('click', evt => {
     createTitleSlide();
 });
 
@@ -166,32 +235,34 @@ txtFiledBtn.addEventListener('click', evt => {
 
 // --- Functions ------------------------------------------------------
 
-function makeList(){
+function makeList() {
     lastClickedElm.innerHTML = `<ul><li>Your text here ...</li></ul>`;
 }
 
 function createListSlide() {
     let slide = new Slide("list");
     slide.addBulletList();
-    presentation.presentation.slides.push(slide);
+    presentation.slides.push(slide.getSlide());
 
     let divs = listSlideTemplate.content.querySelectorAll("div");
-    let ul = listSlideTemplate.content.querySelectorAll("li")
+    let ul = listSlideTemplate.content.querySelectorAll("li");
 
     divs[1].innerHTML = slide.getSlide().headline.text;
     ul[0].innerHTML = slide.getSlide().textBoxes[0].text[0];
     ul[1].innerHTML = slide.getSlide().textBoxes[1].text[0];
 
     let div = listSlideTemplate.content.cloneNode(true);
-    div.addEventListener('change', localSave);
+    for (let i = 0; i < div.firstElementChild.children.length; ++i) {
+        div.firstElementChild.children[i].addEventListener('input', localSave);
+    }
     editSlideCont.appendChild(div);
 }
 
-function createTxtAndImgSlide(){
+function createTxtAndImgSlide() {
     let slide = new Slide("txtAndImg");
     slide.addText();
     slide.addImg();
-    presentation.presentation.slides.push(slide.getSlide());
+    presentation.slides.push(slide.getSlide());
 
     let divs = txtAndImgTemplate.content.querySelectorAll("div");
     let img = txtAndImgTemplate.content.querySelector("img");
@@ -201,23 +272,60 @@ function createTxtAndImgSlide(){
     img.src = slide.getSlide().img.src;
 
     let div = txtAndImgTemplate.content.cloneNode(true);
+    for (let i = 0; i < div.firstElementChild.children.length; ++i) {
+        div.firstElementChild.children[i].addEventListener('input', localSave);
+    }
     editSlideCont.appendChild(div);
 }
 
-function createTitleSlide(){
+function createTitleSlide() {
     let slide = new Slide("title");
-    presentation.presentation.slides.push(slide.getSlide());
-    
+    presentation.slides.push(slide.getSlide());
+
     let divs = titleTemplate.content.querySelectorAll("div");
     divs[1].innerHTML = slide.getSlide().headline.text;
     divs[2].innerHTML = slide.getSlide().byLine.text;
-    
+
     let div = titleTemplate.content.cloneNode(true);
+    for (let i = 0; i < div.firstElementChild.children.length; ++i) {
+        div.firstElementChild.children[i].addEventListener('input', localSave);
+    }
+
     editSlideCont.appendChild(div);
 }
 
-function localSave(){
-    presentation.presentation.slides[0].textBoxes[0].text = 
+function localSave(evt) {
+    let currentDiv = evt.target;
+    let containerDiv = currentDiv.parentNode;
+    console.log(currentDiv);
+    // Get the informations on the div
+    // Save them in the corresponding slide in local storage
+
+    for (let i = 0; i < containerDiv.childNodes.length; ++i) {
+        let className = containerDiv.childNodes[i].className;
+        if (className) {
+            className = className.split(" ")[0];
+        }
+        console.log(className);
+
+        switch (className) {
+            case "title": {
+                console.log(presentation.slides[currentSlide]);
+                presentation.slides[currentSlide].headline.text = containerDiv.childNodes[i].innerHTML;
+                break;
+            }
+            case "byFrame" : {
+                presentation.slides[currentSlide].byLine.text = containerDiv.childNodes[i].innerHTML;
+                break;
+            }
+            case "imgContainer": {
+                presentation.slides[currentSlide].img.src = containerDiv.childNodes[i].childNodes[0].src;
+                break;
+            }
+            default: {}
+        }
+    }
 
     localStorage.setItem("presentation", JSON.stringify(presentation));
 }
+
